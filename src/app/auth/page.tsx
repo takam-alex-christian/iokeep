1de71245584dcfb533773c68892051efc080556e
takam <takamalexchristian@gmail.com>
2023-08-29 13:51:55 +0800

    more code refactoring

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,29 +3,54 @@
 
 import React, { useEffect, useState } from "react"
 
+//next
+import { redirect } from "next/navigation"
+
+//components
 import PrimaryButton from "@/components/PrimaryButton"
 import InputField from "@/components/InputField"
 import Heading from "@/components/Heading"
 import LinkButton from "@/components/LinkButton"
 
 
+
 //lib
-import { isUsernameAvailable, signUp } from "@/libs/auth"
+import { isUsernameAvailable, signIn, signUp } from "@/libs/auth"
 import ErrorComponent from "@/components/ErrorComponent"
+
+//validation rules
+const validationRulesObject = {
+    //this object will allow for easy rule change later down the line. wh
+    //will save me some time not having to look for these individual would be magic numbers to change them
+    username: {
+        minLength: 1,
+        maxLength: 12,
+        allowedRegexp: ""
+    },
+    password: {
+        minLength: 8,
+        maxLength: 24
+    }
+}
 
 interface AuthPageStateType {
 
     activeForm: "sign_in" | "sign_up",
-
+    authenticated: boolean
 }
 
 export default function Auth() {
 
     const [authPageState, setAuthPageState] = useState<AuthPageStateType>(
-        { activeForm: "sign_in" }
+        {
+            activeForm: "sign_in",
+            authenticated: false
+        }
     )
 
-
+    useEffect(()=>{
+        if(authPageState.authenticated == true) redirect("/app");
+    }, [authPageState.authenticated])
 
     return (
         <main className="w-screen h-screen">
@@ -37,12 +62,23 @@ export default function Auth() {
     )
 }
 
-
 function SignInForm(props: { authPageState: AuthPageStateType, setAuthPageState: React.Dispatch<React.SetStateAction<AuthPageStateType>> }) {
+
 
     const [formState, setFormState] = useState({
         usernameValue: "",
         passwordValue: ""
+    })
+
+    const [formValidationState, setFormValidationState] = useState({
+        username: {
+            isValid: false as boolean,
+            errors: [] as string[]
+        },
+        password: {
+            isValid: false as boolean,
+            errors: [] as string[]
+        }
     })
 
     useEffect(() => {
@@ -50,31 +86,97 @@ function SignInForm(props: { authPageState: AuthPageStateType, setAuthPageState:
     }, [formState])
 
     function usernameChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+
+        let isValid: boolean = false;
+        let errors: string[] = []
+
+        if (e.target.value.length == 0) {
+            errors.push("Username can not be empty")
+        } else {
+            if (e.target.value.length >= validationRulesObject.username.minLength && e.target.value.length <= validationRulesObject.username.maxLength) {
+                isValid = true
+            } else {
+                errors.push(`Username should be between ${validationRulesObject.username.minLength} and ${validationRulesObject.username.maxLength} characters long`)
+            }
+        }
+
+        //we update validationstate
+        setFormValidationState((prevState) => {
+            return { ...prevState, username: { errors, isValid } }
+        })
+
+        //we update formstate
         setFormState((prevState) => {
             return { ...prevState, usernameValue: e.target.value }
         })
     }
 
     function passwordChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+
+        let isValid: boolean = false;
+        let errors: string[] = []
+
+        if (e.target.value.length == 0) {
+            errors.push("Password can not be empty")
+        } else {
+            isValid = true
+        }
+
+        //we update validationstate
+        setFormValidationState((prevState) => {
+            return { ...prevState, password: { errors, isValid } }
+        })
+
         setFormState((prevState) => {
             return { ...prevState, passwordValue: e.target.value }
         })
     }
 
+    async function signinFormSubmitHandler(e: React.FormEvent) {
+        e.preventDefault();
+
+        if (formValidationState.username.isValid && formValidationState.password.isValid) {
+            //login request
+            await signIn({ username: formState.usernameValue, password: formState.passwordValue }).then((jsonResponse) => {
+
+                //if we succeed we redirect to app
+                if (jsonResponse.succeeded == true) {
+                    
+                    console.log("signin successful. what next ?")
+
+                    props.setAuthPageState((prevState)=>{
+                        return {...prevState, authenticated: true}
+                    });
+                }
+
+            })
+        }
+
+    }
     return (
         <div className="flex flex-col gap-4 max-w-sm w-full">
 
             <Heading label="Sign in to Iokeep" />
 
-            <form className="flex flex-col gap-4">
+            <form onSubmit={signinFormSubmitHandler} className="flex flex-col gap-4">
 
                 <InputField placeholder={"Username"} onChangeHandler={usernameChangeHandler} />
 
+                {formValidationState.username.isValid == false && formValidationState.username.errors.length > 0 &&
+                    formValidationState.username.errors.map((eachError, index) => {
+                        return <ErrorComponent key={index} label={eachError} />
+                    })
+                }
 
                 <InputField placeholder={"Pa***ord"} onChangeHandler={passwordChangeHandler} />
 
+                {formValidationState.password.isValid == false && formValidationState.password.errors.length > 0 &&
+                    formValidationState.password.errors.map((eachError, index) => {
+                        return <ErrorComponent key={index} label={eachError} />
+                    })
+                }
 
-                <PrimaryButton label={"Sign In"} />
+                <PrimaryButton disabled={!(formValidationState.username.isValid && formValidationState.password.isValid)} label={"Sign In"} />
 
             </form>
 
@@ -88,22 +190,6 @@ function SignInForm(props: { authPageState: AuthPageStateType, setAuthPageState:
 }
 
 function SignUpForm(props: { authPageState: AuthPageStateType, setAuthPageState: React.Dispatch<React.SetStateAction<AuthPageStateType>> }) {
-
-    //validation rules
-    const validationRulesObject = {
-        //this object will allow for easy rule change later down the line. wh
-        //will save me some time not having to look for these individual would be magic numbers to change them
-        username: {
-            minLength: 1,
-            maxLength: 12,
-            allowedRegexp: ""
-        },
-        password: {
-            minLength: 8,
-            maxLength: 24
-        }
-    }
-
     //formstate adapted to accomodate errorState for each individual form
     //both *HasError && *ErrorMessage are set on field validation
     //only *HasError are validated on formsubmition
@@ -128,7 +214,7 @@ function SignUpForm(props: { authPageState: AuthPageStateType, setAuthPageState:
         }
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(formValidationState)
     }, [formValidationState])
 
@@ -140,21 +226,21 @@ function SignUpForm(props: { authPageState: AuthPageStateType, setAuthPageState:
         let isValid: boolean = false
 
         if (e.target.value.length >= validationRulesObject.username.minLength && e.target.value.length <= validationRulesObject.username.maxLength) {
-            
-            await isUsernameAvailable(e.target.value).then((jsonResponse)=>{
+
+            await isUsernameAvailable(e.target.value).then((jsonResponse) => {
                 console.log(jsonResponse)
-                if(jsonResponse.available == true){
+                if (jsonResponse.available == true) {
                     isValid = true
-                }else {
+                } else {
                     errorArray.push("username " + e.target.value + " is not available for use. pick another one")
                 }
             })
-    
-        } else{
+
+        } else {
             errorArray.push("length should be between " + validationRulesObject.username.minLength + " and " + validationRulesObject.username.maxLength + " characters")
         }
 
-        
+
         setFormValidationState((prevState) => {
             return { ...prevState, username: { errors: errorArray, isValid: isValid } }
         })
@@ -174,10 +260,10 @@ function SignUpForm(props: { authPageState: AuthPageStateType, setAuthPageState:
 
         if (e.target.value.length >= validationRulesObject.password.minLength && e.target.value.length <= validationRulesObject.password.maxLength) {
             isValid = true
-        }else {
+        } else {
             errorArray.push("length should be between " + validationRulesObject.password.minLength + " and " + validationRulesObject.password.maxLength + " characters")
         }
-        
+
         setFormValidationState((prevState) => {
             return { ...prevState, password: { errors: errorArray, isValid: isValid } }
         })
@@ -195,37 +281,45 @@ function SignUpForm(props: { authPageState: AuthPageStateType, setAuthPageState:
 
         if (formState.createPasswordValue == e.target.value) {
             isValid = true;
-        }else{
+        } else {
             errorArray.push("Both passowrds must be exactly the same")
 
         }
 
-        
+
         setFormValidationState((prevState) => {
             return { ...prevState, confirmPassword: { errors: errorArray, isValid } }
         })
-        
+
         setFormState((prevState) => {
             return { ...prevState, confirmPasswordValue: e.target.value }
         })
     }
 
-    function formSubmitHandler(e: React.FormEvent) {
+    async function formSubmitHandler(e: React.FormEvent) {
         e.preventDefault();
 
         if (formValidationState.username.isValid && formValidationState.password.isValid && formValidationState.confirmPassword.isValid) {
-            signUp({
+            await signUp({
                 username: formState.usernameValue,
                 password: formState.confirmPasswordValue
             }).then((response) => {
+
                 console.log(response)
+                //we use local storage to store the auth token
+
+                //we updated the page's authenticated state
+                props.setAuthPageState((prevState)=>{
+                    return {...prevState, authenticated: true}
+                })
+
+                //on subsequent launches, we can just check wether _authToken exist in local memory
+
             })
 
-            console.log("formSubmited")
         } else {
             console.log("form was not send because some fields are not yet validated")
         }
-
 
     }
 
