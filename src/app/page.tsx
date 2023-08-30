04231d98e76c2c3614569e84ed3535a5a17a7017
takam <takamalexchristian@gmail.com>
2023-08-30 10:48:17 +0800

    more code refactoring on signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
 import {cookies} from "next/headers"
-export default function Home() {
 
-  console.log(cookies().getAll())
+export default function Home() {
+//test code
+  const _authToken = cookies().get("_authToken")
+  
   return (
     <div>
-      new feature comming soon <br />
+      new feature comming soon {_authToken?.value} <br />
       you now have to authenticate before login in
     </div>
   )
