6b8df156723ea86d8495710234788fc2b0490a11
takam <takamalexchristian@gmail.com>
2023-09-06 10:28:01 +0800

    search bar placeholder changed to coming soon

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
                 <div className="flex flex-row justify-start border  border-slate-200 h-12 rounded-full">
                     <label className="flex flex-row gap-4 h-full w-full px-4 items-center">
                         <div ><SearchNormal1 size="24" color="#474E41" variant="Outline" /></div>
-                        <div className="flex-1"><input type="search" className="w-full focus:outline-none bg-inherit" placeholder="Search" /></div>
+                        <div className="flex-1"><input type="search" className="w-full focus:outline-none bg-inherit" placeholder="Search feature coming soon..." /></div>
                     </label>
                 </div>
             </form>
