29228fe45796bce1430067adbf89b6106b2effcb
takam <takamalexchristian@gmail.com>
2023-09-06 18:59:51 +0800

    notedate last modified display format changed

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -10,6 +10,33 @@ type NoteProps = {
 
 export default function Note(props: NoteProps) {
 
+    const daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+
+    const monthsOfTheYear = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+    
+    const noteDate = new Date(props.noteData.lastModified)
+    const DateString = `${daysOfTheWeek[noteDate.getDay()]}, ${monthsOfTheYear[noteDate.getMonth()]} ${noteDate.getDate()}, ${noteDate.getFullYear()}`
     
     return (
         <article className=' rounded-2xl bg-light-gray'>
@@ -19,11 +46,9 @@ export default function Note(props: NoteProps) {
 
                 <p className='font-light text-base text-gray'>{props.noteData.body}</p>
 
-                <small className='font-light text-sm text-gray'>{props.noteData.lastModified.toString()}</small>
+                <small className='font-light text-sm text-gray'>{DateString}</small>
             </div>
-            <div> {/* Note click overlay*/}
-
-            </div>
+            
         </article>
     )
 }
