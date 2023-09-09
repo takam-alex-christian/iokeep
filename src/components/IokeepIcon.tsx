d1b54b33a0f36a0ce5b45005e0f7cd943d874a5e
takam <takamalexchristian@gmail.com>
2023-09-10 03:17:47 +0800

    current logo component

---
+++ src/components/IokeepIcon.tsx
@@ -0,0 +1,11 @@
+
+import { Notepad2} from "iconsax-react"
+
+
+
+export default function IokeepIcon() {
+
+    return (
+        <div className="flex flex-row gap-4 text-neutral-800"><Notepad2 color="#000" /><h3 className="text-lg font-semibold">Iokeep</h3></div>
+    )
+}
