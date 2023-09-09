5d912f45be8f6447102ef3f229536b05e2c03614
takam <takamalexchristian@gmail.com>
2023-09-09 22:11:11 +0800

    InputField component, hide/show feature implemented

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -42,9 +42,9 @@ export default function Note(props: NoteProps) {
         <article className=' rounded-2xl bg-light-gray'>
             
             <div className='flex flex-col p-5 gap-3'>{/* Note container*/}
-                <h3 className='font-medium text-lg text-dark'>{props.noteData.title}</h3>
+                <h3 className='font-medium text-lg text-dark overflow-hidden text-ellipsis'>{props.noteData.title}</h3>
 
-                <p className='font-light text-base text-gray'>{props.noteData.body}</p>
+                <p className='font-light text-base text-gray overflow-hidden text-ellipsis'>{props.noteData.body}</p>
 
                 <small className='font-light text-sm text-gray'>{DateString}</small>
             </div>
