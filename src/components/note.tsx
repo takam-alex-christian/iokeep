51d9adb202a025940a1d0b0b5cd61b92c4ae4620
takam <takamalexchristian@gmail.com>
2023-09-09 23:27:08 +0800

    note footer added.

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -34,21 +34,26 @@ export default function Note(props: NoteProps) {
         "November",
         "December"
     ]
-    
+
     const noteDate = new Date(props.noteData.lastModified)
     const DateString = `${daysOfTheWeek[noteDate.getDay()]}, ${monthsOfTheYear[noteDate.getMonth()]} ${noteDate.getDate()}, ${noteDate.getFullYear()}`
-    
+
     return (
         <article className=' rounded-2xl bg-light-gray'>
-            
-            <div className='flex flex-col p-5 gap-3'>{/* Note container*/}
-                <h3 className='font-medium text-lg text-dark overflow-hidden text-ellipsis'>{props.noteData.title}</h3>
 
-                <p className='font-light text-base text-gray overflow-hidden text-ellipsis'>{props.noteData.body}</p>
+            <div className='flex flex-col p-5 gap-3 justify-between h-full'>{/* Note container*/}
+                <div className='flex flex-col gap-3'>
+                    <h3 className='font-medium text-lg text-dark overflow-hidden text-ellipsis'>{props.noteData.title}</h3>
 
-                <small className='font-light text-sm text-gray'>{DateString}</small>
+                    <p className='font-light text-base text-gray overflow-hidden text-ellipsis'>{props.noteData.body}</p>
+                </div>
+
+                {/* note footer */}
+                <div className={""} role='footer'>
+                    <small className='font-light text-sm text-gray'>{DateString}</small>
+                </div>
             </div>
-            
+
         </article>
     )
 }
