e71956c2ba42854d0e5887ca7369328a7f83066f
takam <takamalexchristian@gmail.com>
2023-09-17 20:38:04 +0800

    implementing the signin logic and restructuring states and the dom

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,6 +1,7 @@
 
 "use client"
 
+
 import Heading from "@/components/Heading"
 import ErrorComponent from "@/components/ErrorComponent"
 import PrimaryButton from "@/components/PrimaryButton"
@@ -11,8 +12,11 @@ import Row from "@/layouts/Row"
 import React, { HTMLInputTypeAttribute, useEffect, useState, useReducer } from "react"
 import Link from "next/link"
 
+import { useRouter } from "next/navigation"
+
 //loaders
 import BeatLoader from "react-spinners/BeatLoader";
+import { signIn } from "@/libs/auth"
 
 
 
@@ -20,9 +24,11 @@ import BeatLoader from "react-spinners/BeatLoader";
 //sign in form
 export default function SignInPage() {
 
-    return (
-        <main className="min-h-screen flex justify-center items-center">
 
+
+    return (
+        <main className="relative max-sm:py-6 md:min-h-screen flex justify-center items-center px-4">
+            {/* error area */}
             <SignInForm />
 
         </main>
@@ -33,26 +39,42 @@ export default function SignInPage() {
 type SignInFormStateType = {
     usernameValue: string,
     passwordValue: string,
+
+    error: string,
+    showError: boolean,
+
     isLoading: boolean
 }
 
-type SignInFormReducerAction = { type: "update_username", payload: { value: string } } | { type: "update_password", payload: { value: string } } | {type: "start_loading"} | {type: "stop_loading"}
+type SignInFormReducerAction = { type: "update_username", payload: { value: string } }
+    | { type: "update_password", payload: { value: string } }
+    | { type: "update_error", payload: { value: string } }
+    | { type: "show_error" }
+    | { type: "hide_error" }
+    | { type: "start_loading" }
+    | { type: "stop_loading" }
 
 function SignInForm() {
 
+    const router = useRouter()
 
     const [formState, formDispatch] = useReducer<React.Reducer<SignInFormStateType, SignInFormReducerAction>>((prevState: SignInFormStateType, action: SignInFormReducerAction) => {
         switch (action.type) {
             case "update_username": return { ...prevState, usernameValue: action.payload.value };
             case "update_password": return { ...prevState, passwordValue: action.payload.value };
 
-            case "start_loading": return {...prevState, isLoading: true};
-            case "stop_loading": return {...prevState, isLoading: false};
+            case "update_error": return { ...prevState, error: action.payload.value, showError: true };
+
+            case "start_loading": return { ...prevState, isLoading: true };
+            case "stop_loading": return { ...prevState, isLoading: false };
+
+            case "show_error": return { ...prevState, showError: true };
+            case "hide_error": return { ...prevState, showError: false };
 
             default: return prevState
         }
 
-    }, { usernameValue: "", passwordValue: "", isLoading: false })
+    }, { usernameValue: "", passwordValue: "", error: "", showError: false, isLoading: false })
 
     //test code//
     useEffect(() => {
@@ -60,33 +82,68 @@ function SignInForm() {
     }, [formState])
     //end of test code //
 
-    function formSubmitHandler(e: React.FormEvent){
+    async function formSubmitHandler(e: React.FormEvent) {
         e.preventDefault();
 
-        formDispatch({type: "start_loading"});
+        formDispatch({ type: "start_loading" });
 
-        //post login data to server
+        //we validate the form input here
 
-        //on response, update the formstate
-        
+        if (formState.usernameValue.length > 0) {
+            if (formState.passwordValue.length > 0) {
+                //we can pursue signing in the user
+
+                //post login data to server
+                console.log("life is good")
+                //on response, update the formstate
+
+                await signIn({ username: formState.usernameValue, password: formState.passwordValue }).then((jsonResponse) => {
+                    
+
+                    //if we succeed we redirect to app
+                    if (jsonResponse.succeeded == true) {
+
+                        console.log("authed")
+
+                        formDispatch({type: "stop_loading"});
+                        
+                        router.push("/app");
+
+                    }
+
+                })
+
+            } else formDispatch({ type: "update_error", payload: { value: "Password can not be empty!" } })
+        } else formDispatch({ type: "update_error", payload: { value: "Username should not be empty!" } })
+
+
     }
+
+
     return (
         <div className="max-w-md w-full">
             <form className="" method="post" onSubmit={formSubmitHandler}>
                 <Col>
                     <Heading label="IOKEEP | Sign In" />
-                    <Col gap={6}>
-                        <AuthField purpose="username" formState={formState} formDispatch={formDispatch} />
-                        <AuthField purpose="password" formState={formState} formDispatch={formDispatch} />
+                    <Col gap={4}>
+                        <Col gap={2}>
+                            <UsernameField formState={formState} formDispatch={formDispatch} />
+                            <PasswordField formState={formState} formDispatch={formDispatch} />
+                            {/* error block */}
+                            <div>
+                                {formState.showError && <ErrorComponent label={formState.error} />}
+                            </div>
+                        </Col>
+
                         <PrimaryButton>
-                            {formState.isLoading && <BeatLoader cssOverride={{display: "flex", justifyContent: "center", alignItems: "center"}}
+                            {formState.isLoading && <BeatLoader cssOverride={{ display: "flex", justifyContent: "center", alignItems: "center" }}
                                 color="#ffffffa0"
-                                size={12}
+                                size={8}
                             />}
                             {
                                 formState.isLoading == false && <span>Sign In</span>
                             }
-                            
+
                         </PrimaryButton>
 
                         <div className="flex flex-row gap-4 items-center justify-center text-center">
@@ -100,55 +157,15 @@ function SignInForm() {
     )
 }
 
-function AuthField(props: { formState: SignInFormStateType,formDispatch: React.Dispatch<SignInFormReducerAction>, purpose: "password" | "username" }) {
 
-    let label: string | string[] = props.purpose.split("");
-    label.splice(0, 1, label[0].toUpperCase());
-    label = label.join("");
-
-    const [authFieldState, setAuthState] = useState<{ value: string, showError: boolean, error: string }>({
-        value: "",
-
-        showError: false,
-        error: ""
-    });
-
-    ////test code
-    useEffect(() => {
-        console.log(authFieldState)
-    }, [authFieldState])
-    ///end of test code ///
+//password
+function UsernameField(props: { formState: SignInFormStateType, formDispatch: React.Dispatch<SignInFormReducerAction> }) {
 
     function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
         e.preventDefault()
 
-        let { isValid, error } = validate(e.target.value)
+        props.formDispatch({ type: `update_username`, payload: { value: e.target.value } })
 
-        setAuthState((prevState) => {
-            return { ...prevState, value: e.target.value, showError: !isValid, error: error! }
-        })
-
-    }
-
-    function validate(providedValue?: string): { isValid: boolean, error: string | null } {
-
-        let value: string = ""
-
-        if (typeof (providedValue) !== "undefined") value = providedValue
-        else value = authFieldState.value
-
-        if (value.length !== 0) {
-            return { isValid: true, error: null }
-        } else return { isValid: false, error: `${props.purpose}` + " can not be empty!" }
-    }
-
-    function blurHandler(e: React.SyntheticEvent<HTMLInputElement>) {
-
-        e.preventDefault();
-
-        let { isValid } = validate()
-
-        if (isValid) props.formDispatch({ type: `update_${props.purpose}`, payload: { value: authFieldState.value } })
     }
 
     return (
@@ -156,28 +173,59 @@ function AuthField(props: { formState: SignInFormStateType,formDispatch: React.D
 
             <Row className={`overflow-hidden outline outline-1 outline-neutral-300 rounded-2xl focus-within:outline focus-within:outline-1 focus-within:outline-green-300`}>
                 <input
-                    id={props.purpose}
-                    type={props.purpose == "username" ? "text" : props.purpose}
-                    name={props.purpose}
-                    value={authFieldState.value}
-                    placeholder={props.purpose}
-                    aria-placeholder={props.purpose}
+                    type={"text"}
+                    name={"username"}
+                    value={props.formState.usernameValue}
+                    placeholder={"@Username"}
+                    aria-placeholder={"Enter your username"}
 
                     disabled={props.formState.isLoading}
 
                     onChange={changeHandler}
-                    onBlur={blurHandler}
-                    autoComplete={props.purpose == "password" ? "current-password" : "username"}
 
-                    title={label}
+                    autoComplete={"username"}
+
+                    title={"Enter your username!"}
 
                     className={`w-full p-4 text-base font-semibold focus:outline-none`}
                 />
                 <div></div> {/* icon */}
             </Row>
-            {
-                authFieldState.showError && <ErrorComponent label={authFieldState.error} />
-            }
+        </Col>
+    )
+}
+
+//password
+function PasswordField(props: { formState: SignInFormStateType, formDispatch: React.Dispatch<SignInFormReducerAction> }) {
+
+    function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        e.preventDefault()
+
+        props.formDispatch({ type: `update_password`, payload: { value: e.target.value } })
+
+    }
+
+    return (
+        <Col gap={2}>
+
+            <Row className={`overflow-hidden outline outline-1 outline-neutral-300 rounded-2xl focus-within:outline focus-within:outline-1 focus-within:outline-green-300`}>
+                <input
+                    type={"password"}
+                    name={"password"}
+                    value={props.formState.passwordValue}
+                    placeholder={"Password"}
+                    aria-placeholder={"Enter your password!"}
+
+                    disabled={props.formState.isLoading}
+
+                    onChange={changeHandler}
+
+                    autoComplete={"current-password"}
+
+                    className={`w-full p-4 text-base font-semibold focus:outline-none`}
+                />
+                <div></div> {/* icon */}
+            </Row>
         </Col>
     )
 }
