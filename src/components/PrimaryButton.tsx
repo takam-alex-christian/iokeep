a9e58c5c976ed95566147e8fdeb9c427561aa27b
takam <takamalexchristian@gmail.com>
2023-09-17 20:38:41 +0800

    giving the primary button a standart heiht

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -9,7 +9,7 @@ export default function PrimaryButton(props: {
 }) {
 
   return (
-    <button type={"submit"} disabled={props.disabled}  className={`w-full py-4 overflow-hidden px-8 rounded-2xl ${props.disabled? "bg-neutral-600 text-neutral-300" : "bg-green-600 text-neutral-100 "}`}>
+    <button type={"submit"} disabled={props.disabled}  className={`w-full h-14 py-4 overflow-hidden px-8 rounded-2xl ${props.disabled? "bg-neutral-600 text-neutral-300" : "bg-green-600 text-neutral-100 "}`}>
         {props.label}
         {props.children}
     </button>
