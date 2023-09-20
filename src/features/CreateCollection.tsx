a1a0d7d925418de49261a1e4452da052470cba77
takam <takamalexchristian@gmail.com>
2023-09-20 18:27:51 +0800

    validation method changed and state reorganised.

--- src/features/CreateCollection.tsx
+++ src/features/CreateCollection.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useRef, useEffect, useContext } from "react"
+import React, { useState, useRef, useEffect, useContext, useReducer } from "react"
 
 import { appDataContext, appUiContext } from "@/libs/contexts";
 
@@ -11,21 +11,51 @@ import { CollectionDataType } from "@/libs/Types";
 import { postCollectionToBackend } from "@/libs/postDataToBackend";
 
 import ErrorComponent from "@/components/ErrorComponent";
+import Col from "@/layouts/Col";
+import PrimaryButton from "@/components/PrimaryButton";
+
+type MessageType = "success" | "warning" | "error" | null
+
+type CollectionFormStateType = {
+    collectionNameValue: string,
+
+    isLoading: boolean,
+    showMessage: boolean,
+    message: string,
+    messageType: MessageType,
+}
+
+type CollectionFormReducerActionType = { type: "update_collectionName", payload: { value: string } }
+    | { type: "start_loading" }
+    | { type: "stop_loading" }
+    | { type: "show_message" }
+    | { type: "hide_message" }
+
+    | { type: "update_message", payload: { value: string, type: MessageType } }
 
 export default function CreateCollection() {
+
+    const [collectionState, collectionDispatch] = useReducer((prevState: CollectionFormStateType, action: CollectionFormReducerActionType) => {
+        switch (action.type) {
+            case "update_collectionName": return { ...prevState, collectionNameValue: action.payload.value }
+            
+            case "start_loading": return { ...prevState, isLoading: true }
+            case "stop_loading": return { ...prevState, isLoading: false }
+            
+            case "show_message": return { ...prevState, showMessage: true }
+            case "hide_message": return { ...prevState, showMessage: false }
+            
+            case "update_message": return { ...prevState, message: action.payload.value, messageType: action.payload.type, showMessage: true }
+            default: return prevState;
+        }
+    }, { collectionNameValue: "", message: "", messageType: null, showMessage: false, isLoading: false } as CollectionFormStateType)
 
     const { collectionsData, isLoading: isCollectionsDataLoading, setCollectionsData } = useCollections()
 
     const { appDataDispatch, appDataState } = useContext(appDataContext)
     const { appUiDispatch, appUiState } = useContext(appUiContext)
 
-    const [formState, setFormState] = useState({ collectionName: "" })
-
-    const [formValidation, setFormValidation] = useState({
-        isCollectionNameValid: false,
-        showError: false,
-        errors: [""]
-    })
+    // const [formState, setFormState] = useState<{ collectionNameValue: string, showMessage: boolean, message: string, isError: false }>({ collectionNameValue: "", isError: false, showMessage: false, message: "" })
 
     const collectionNameRef = useRef<HTMLInputElement>(null);
 
@@ -33,58 +63,61 @@ export default function CreateCollection() {
         collectionNameRef.current?.focus();
     }, [])
 
-    useEffect(() => {
-        let collectionValidationObject = validateCollectionName();
-
-        setFormValidation((prevState) => {
-            return { ...prevState, isCollectionNameValid: collectionValidationObject.isValid, errors: collectionValidationObject.errors }
-        })
-
-    }, [formState.collectionName])
-
-    function validateCollectionName() {
-        let isValid: boolean = true;
-        let errors: string[] = [];
-
-        if (formState.collectionName.length == 0) {
-            isValid = false;
-            errors.push(" collection name cannot be empty")
-        }
-
-        collectionsData?.collections.forEach((eachCollection) => {
-            if (eachCollection.collectionName.toLowerCase() == formState.collectionName.toLowerCase()) {
-                isValid = false;
-                errors.push(`A collection already exist with the name ${formState.collectionName}`);
-            }
-        })
-
-        return { isValid, errors }
-    }
-
     function formSubmitHandler(e: React.FormEvent) {
+        
+        //show with a loader that the request is being processed
+        collectionDispatch({type:"start_loading"})
+
+        let message: string = ""
+        let messageType: MessageType = null
 
         e.preventDefault();
 
-        setCollectionsData({ ...collectionsData, collections: [...(collectionsData ? collectionsData.collections.slice(0) : []), { collectionName: formState.collectionName, _collectionId: "" }] })
 
-        postCollectionToBackend({
-            collectionName: formState.collectionName,
-            _collectionId: ""
-        }).then((result) => {
-            console.log(result)
-            // mutate("/collections")
-        })
+        if (collectionState.collectionNameValue.length > 0) {
+            //life is even better
+            collectionsData?.collections.forEach((eachCollection) => {
+                if (eachCollection.collectionName.toLowerCase() == collectionState.collectionNameValue.toLowerCase()) {
 
-        appUiDispatch({ type: "hide_modal" });
+                    message = `A collection already exist with the name ${collectionState.collectionNameValue}`
+                    messageType = "error"
+                }
+            })
+
+            //validated, what do we do now
+
+            if (messageType !== "error") {
+
+                setCollectionsData({ ...collectionsData, collections: [...(collectionsData ? collectionsData.collections.slice(0) : []), { collectionName: collectionState.collectionNameValue, _collectionId: "" }] })
+
+                postCollectionToBackend({
+                    collectionName: collectionState.collectionNameValue,
+                    _collectionId: ""
+                }).then((result) => {
+                    console.log(result)
+                    mutate("/collections")
+                })
+
+                appUiDispatch({ type: "hide_modal" });
+
+            } else {
+                collectionDispatch({ type: "update_message", payload: { value: message, type: messageType } })
+            }
+
+        } else {
+            collectionDispatch({ type: "update_message", payload: { value: "Collection name can not be empty!", type: "error" } })
+        }
+
+
+        collectionDispatch({type:"stop_loading"})
 
     }
 
     function collectionNameChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
 
-        setFormState((prev) => {
-            if (prev.collectionName.length > 0) setFormValidation((prevState) => { return { ...prevState, showError: true } })
-            return { ...prev, collectionName: e.target.value }
-        })
+        e.preventDefault();
+
+        collectionDispatch({type: "update_collectionName", payload: {value: e.target.value}})
 
     }
 
@@ -92,24 +125,25 @@ export default function CreateCollection() {
         <div className={"rounded-3xl max-w-lg w-full p-4 shadow-xl " + `${appUiState.uiMode == "light" ? "bg-zinc-100 " : "bg-zinc-800"}`}>
             <form action="" onSubmit={formSubmitHandler}>
                 <div className="flex flex-col gap-4">
-                    <div
-                        className={" rounded-2xl overflow-hidden outline outline-1  " + `${appUiState.uiMode == "light" ? "outline-zinc-300  focus-within:shadow-md " : "outline-zinc-700 focus-within:shadow-2xl"}`}>
+                    <Col gap={2}>
+                        <div
+                            className={" rounded-2xl overflow-hidden outline outline-1  " + `${appUiState.uiMode == "light" ? "outline-zinc-300  focus-within:shadow-md " : "outline-zinc-700 focus-within:shadow-2xl"}`}>
 
-                        <input
-                            ref={collectionNameRef}
-                            type="text"
-                            onChange={collectionNameChangeHandler}
-                            value={formState.collectionName}
-                            placeholder="Collection Name"
-                            className={"w-full p-4 text-base font-semibold focus:outline-none bg-transparent " + `${appUiState.uiMode == "light" ? "text-zinc-900 placeholder:text-zinc-400" : "text-zinc-500 placeholder:text-zinc-700"}`} />
-                    </div>
+                            <input
+                                ref={collectionNameRef}
+                                type="text"
+                                onChange={collectionNameChangeHandler}
+                                value={collectionState.collectionNameValue}
+                                placeholder="Collection Name"
+                                className={"w-full p-4 text-base font-semibold focus:outline-none bg-transparent " + `${appUiState.uiMode == "light" ? "text-zinc-900 placeholder:text-zinc-400" : "text-zinc-500 placeholder:text-zinc-700"}`} />
+                        </div>
 
-                    {formValidation.isCollectionNameValid == false && formValidation.showError && formValidation.errors.map((eachError, index) => {
-                        return (<ErrorComponent key={index} label={eachError} />)
-                    })}
+                        {collectionState.showMessage && <ErrorComponent label={collectionState.message} />}
+
+                    </Col>
 
                     <div>
-                        <button className={`w-full bg-green-600 text-neutral-100 py-4 px-8 rounded-2xl disabled:bg-neutral-500`} disabled={formValidation.isCollectionNameValid !== true && formValidation.showError == true ? true : false}>Create Collection</button>
+                        <PrimaryButton>{ collectionState.isLoading? "Loading": "Create Collection"}</PrimaryButton>
                     </div>
                 </div>
             </form>
